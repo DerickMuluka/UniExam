9d684bc8d50057700d595763b8a06439ecd02975
Derick Muluka <mulukaderick@gmail.com>
2024-09-16 16:23:57 +0300

    Add header.php with navigation menu and links

---
+++ views/partials/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UniExam</title>
+    <link rel="stylesheet" href="../../css/styles.css">
+</head>
+<body>
+<header>
+    <h1>UniExam</h1>
+    <nav>
+        <ul>
+            <li><a href="http://localhost/uniexam/index.php">Home</a></li>
+            <li><a href="http://localhost/uniexam/views/students/login.php">Students</a></li>
+            <li><a href="http://localhost/uniexam/views/lecturers/login.php">Lecturers</a></li>
+            <li><a href="http://localhost/uniexam/views/auth/login.php?action=logout">Admin</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+
+
+
+
+
+
